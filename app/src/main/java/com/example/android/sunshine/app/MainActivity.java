0687998aa4e364d728d6286bd9c6ff14c9262e64
Amanpreet Singh <apsdehal@gmail.com>
2016-05-01 21:31:05 +0530

    Add show location option to menu

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.android.sunshine.app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +55,32 @@ public class MainActivity extends AppCompatActivity {
             startActivity(settingIntent);
             return true;
         }
+        if (id == R.id.action_location) {
+            openUserLocationInMap();
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void openUserLocationInMap() {
+        final String queryParam = "q";
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String location =
+                prefs.getString(getString(R.string.pref_general_location_key),
+                        getString(R.string.pref_general_location_default_value));
+
+        String baseUrl = "geo:0,0?";
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        Uri uri = Uri.parse(baseUrl).buildUpon()
+                .appendQueryParameter(queryParam, location).build();
+
+        intent.setData(uri);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d(LOG_TAG, "Nothing available to handle map");
+        }
     }
 }
